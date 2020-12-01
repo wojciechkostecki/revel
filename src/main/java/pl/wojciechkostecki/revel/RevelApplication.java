54a98944ee48fdfb34a236c890d745fe7c79b7b7
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-12-01 14:20:15 +0100

    Initial commit

---
+++ src/main/java/pl/wojciechkostecki/revel/RevelApplication.java
@@ -0,0 +1,13 @@
+package pl.wojciechkostecki.revel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wojciechkostecki/revel/RevelApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojciechkostecki.revel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
