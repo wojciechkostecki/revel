0bc4e362dbfb8a257eaad5c24a1b00ad8213fae2
Wojciech Kostecki <wt.kostecki@gmail.com>
2021-01-03 18:42:51 +0100

    Create a MenuItem entity

---
+++ src/main/java/pl/wojciechkostecki/revel/model/MenuItem.java
@@ -0,0 +1,26 @@
+package pl.wojciechkostecki.revel.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class MenuItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String category;
+
+    private String name;
+
+    private String ingredients;
+
+    private double price;
+    
+}
