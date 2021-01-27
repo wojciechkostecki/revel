9adfd358c151acdcf06274d6798434212e6e548c
Wojciech Kostecki <wt.kostecki@gmail.com>
2021-01-27 19:30:27 +0100

    Import change for Logger and LoggerFactory

--- src/main/java/pl/wojciechkostecki/revel/controller/MenuItemController.java
+++ src/main/java/pl/wojciechkostecki/revel/controller/MenuItemController.java
@@ -1,7 +1,8 @@
 package pl.wojciechkostecki.revel.controller;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
