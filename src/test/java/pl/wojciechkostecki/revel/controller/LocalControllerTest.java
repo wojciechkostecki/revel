3c8962da6905b773a289149f2a9e7211eb239792
Wojciech Kostecki <wt.kostecki@gmail.com>
2021-03-03 19:38:43 +0100

    Removing the generated test, creating a new database dedicated to tests and initiating tests for the Local

---
+++ src/test/java/pl/wojciechkostecki/revel/controller/LocalControllerTest.java
@@ -0,0 +1,34 @@
+package pl.wojciechkostecki.revel.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LocalControllerTest {
+
+    @Test
+    void createLocal() {
+    }
+
+    @Test
+    void getAllLocals() {
+    }
+
+    @Test
+    void getLocal() {
+    }
+
+    @Test
+    void getLocalsByName() {
+    }
+
+    @Test
+    void updateLocal() {
+    }
+
+    @Test
+    void deleteLocal() {
+    }
+}
