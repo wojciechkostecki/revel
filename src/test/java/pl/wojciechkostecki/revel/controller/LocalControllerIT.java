4e696da8d997c7dabfe66316529a2e0a52efdf5e
Wojciech Kostecki <wt.kostecki@gmail.com>
2021-03-30 17:23:17 +0200

    Removing unnecessary imports:

--- src/test/java/pl/wojciechkostecki/revel/controller/LocalControllerIT.java
+++ src/test/java/pl/wojciechkostecki/revel/controller/LocalControllerIT.java
@@ -2,7 +2,6 @@ package pl.wojciechkostecki.revel.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.assertj.core.api.Assertions;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
