09b58e356739e9cd6faf9f5291575c144efaede4
Wojciech Kostecki <wt.kostecki@gmail.com>
2021-03-31 17:25:32 +0200

    Creating Exception Handler for EntityNotFoundException

--- src/main/java/pl/wojciechkostecki/revel/controller/GlobalExceptionHandler.java
+++ src/main/java/pl/wojciechkostecki/revel/controller/GlobalExceptionHandler.java
@@ -1,9 +1,23 @@
 package pl.wojciechkostecki.revel.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public final ResponseEntity<Object> handleEntityNotFoundException
+            (EntityNotFoundException exception, WebRequest request) {
+        HttpHeaders headers = new HttpHeaders();
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        return handleExceptionInternal(exception, null, headers, status, request);
+    }
 }
