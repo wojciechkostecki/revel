5d87ceb168e363865459de62530d0c38bc3ca782
48531 <wt.kostecki@gmail.com>
2021-04-09 17:55:32 +0200

    optimize imports

--- src/test/java/pl/wojciechkostecki/revel/controller/MenuItemControllerIT.java
+++ src/test/java/pl/wojciechkostecki/revel/controller/MenuItemControllerIT.java
@@ -1,6 +1,5 @@
 package pl.wojciechkostecki.revel.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
