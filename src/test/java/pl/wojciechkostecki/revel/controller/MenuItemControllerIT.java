e51ffcff6f566e96f70005855aff41a43c868710
48531 <wt.kostecki@gmail.com>
2021-04-09 15:46:55 +0200

    Create a test class for MenuItem Controller

---
+++ src/test/java/pl/wojciechkostecki/revel/controller/MenuItemControllerIT.java
@@ -0,0 +1,35 @@
+package pl.wojciechkostecki.revel.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class MenuItemControllerIT {
+
+    @Test
+    void createMenuItemTest() {
+    }
+
+    @Test
+    void getAllMenuItemsTest() {
+    }
+
+    @Test
+    void getMenuItemsByNameTest() {
+    }
+
+    @Test
+    void updateMenuItemTest() {
+    }
+
+    @Test
+    void deleteMenuItemTest() {
+    }
+}
