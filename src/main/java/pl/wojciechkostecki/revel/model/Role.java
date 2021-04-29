4a98158f356f402f72370b1a42a473057dffba06
48531 <wt.kostecki@gmail.com>
2021-04-29 22:38:00 +0200

    Creating a roles for users

--- src/main/java/pl/wojciechkostecki/revel/model/Role.java
+++ src/main/java/pl/wojciechkostecki/revel/model/Role.java
@@ -1,2 +1,16 @@
-package pl.wojciechkostecki.revel.model;public enum Role {
+package pl.wojciechkostecki.revel.model;
+
+public enum Role {
+    USER("USER"),
+    ADMIN("ADMIN");
+
+    private String value;
+
+    Role(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
--- src/main/java/pl/wojciechkostecki/revel/model/UserRole.java
+++ src/main/java/pl/wojciechkostecki/revel/model/UserRole.java
@@ -1,2 +1,21 @@
-package pl.wojciechkostecki.revel.model;public class UserRole {
+package pl.wojciechkostecki.revel.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserRole {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Role name;
 }
