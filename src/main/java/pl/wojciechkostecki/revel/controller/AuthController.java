61ed56558cdfe7938218a3f7341dc263c22e9f55
48531 <wt.kostecki@gmail.com>
2021-05-26 08:19:08 +0200

    Remove try catch

--- src/main/java/pl/wojciechkostecki/revel/controller/AuthController.java
+++ src/main/java/pl/wojciechkostecki/revel/controller/AuthController.java
@@ -43,17 +43,13 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<UserDTO> login(@RequestBody @Valid UserDTO request) {
-        try {
-            Authentication authenticate = authenticationManager
-                    .authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        Authentication authenticate = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
 
-            UserDetails user = (UserDetails) authenticate.getPrincipal();
-
-            return ResponseEntity.ok()
-                    .header(HttpHeaders.AUTHORIZATION, jwtTokenUtil.generateAccessToken(user))
-                    .body(userMapper.toDto(user));
-        } catch (BadCredentialsException ex) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
+        UserDetails user = (UserDetails) authenticate.getPrincipal();
+        
+        return ResponseEntity.ok()
+                .header(HttpHeaders.AUTHORIZATION, jwtTokenUtil.generateAccessToken(user))
+                .body(userMapper.toDto(user));
     }
 }
