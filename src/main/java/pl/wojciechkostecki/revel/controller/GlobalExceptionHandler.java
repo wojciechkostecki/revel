07da6adf12395794279df119525b2a58ecf57960
48531 <wt.kostecki@gmail.com>
2021-05-28 18:29:25 +0200

    Changing the name of the exception

--- src/main/java/pl/wojciechkostecki/revel/controller/GlobalExceptionHandler.java
+++ src/main/java/pl/wojciechkostecki/revel/controller/GlobalExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import pl.wojciechkostecki.revel.exception.BadRequestException;
-import pl.wojciechkostecki.revel.exception.UserAlreadyExistException;
+import pl.wojciechkostecki.revel.exception.LoginAlreadyUsedException;
 
 import javax.persistence.EntityNotFoundException;
 
@@ -40,9 +40,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(exception, null, headers, status, request);
     }
 
-    @ExceptionHandler(UserAlreadyExistException.class)
-    public final ResponseEntity<Object> handleUserAlreadyExistException
-            (UserAlreadyExistException exception, WebRequest request) {
+    @ExceptionHandler(LoginAlreadyUsedException.class)
+    public final ResponseEntity<Object> handleLoginAlreadyUsedException
+            (LoginAlreadyUsedException exception, WebRequest request) {
         HttpHeaders headers = new HttpHeaders();
         HttpStatus status = HttpStatus.CONFLICT;
         return handleExceptionInternal(exception, null, headers, status, request);
--- src/main/java/pl/wojciechkostecki/revel/exception/UserAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package pl.wojciechkostecki.revel.exception;
-
-public class UserAlreadyExistException extends RuntimeException{
-    public UserAlreadyExistException(String message) {
-        super(message);
-    }
-}
